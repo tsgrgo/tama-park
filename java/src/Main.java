095d46354c53b46a7bc09da0f068304a2b7106f6
tsgrgo <37509858+tsgrgo@users.noreply.github.com>
2026-01-14 23:34:31 +0100

    Update Main.java

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.microedition.io.Connector;
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Connector.setScratchpadFile(new File(""));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        GameApp app = new GameApp();
+        GameScreen screen = new GameScreen(app);
+
+        // --------------------------
+
+        Frame f = new Frame("Game");
+        f.add(screen.unwrap());
+        f.pack();
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+        screen.unwrap().requestFocus();
+
+        f.addWindowListener(new WindowAdapter() {
+            @Override public void windowClosing(WindowEvent e) {
+                f.dispose();
+                System.exit(0);
+            }
+        });
     }
 }
