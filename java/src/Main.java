3c973824e644a9119c5f7fd9e955234da2b0cf03
tsgrgo <37509858+tsgrgo@users.noreply.github.com>
2026-01-15 01:07:53 +0100

    Update Display class

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,3 +1,5 @@
+import com.nttdocomo.ui.Display;
+
 import javax.microedition.io.Connector;
 import java.awt.Frame;
 import java.awt.event.WindowAdapter;
@@ -9,21 +11,6 @@ public class Main {
         Connector.setScratchpadFile(new File(""));
 
         GameApp app = new GameApp();
-
-        // --------------------------
-
-        Frame f = new Frame("Game");
-        f.add(GameApp.canvas.unwrap());
-        f.pack();
-        f.setLocationRelativeTo(null);
-        f.setVisible(true);
-        GameApp.canvas.unwrap().requestFocus();
-
-        f.addWindowListener(new WindowAdapter() {
-            @Override public void windowClosing(WindowEvent e) {
-                f.dispose();
-                System.exit(0);
-            }
-        });
+        Display.init();
     }
 }
--- java/src/com/nttdocomo/ui/Display.java
+++ java/src/com/nttdocomo/ui/Display.java
@@ -1,7 +1,54 @@
 package com.nttdocomo.ui;
 
-public class Display {
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public final class Display {
+
+    private static Canvas current;
+    private static Frame frame;
+
+    // Call once during startup
+    public static void init() {
+        if (frame != null) return;
+
+        frame = new Frame("DoJa Emulator");
+        frame.setSize(240, 240);
+        frame.setLayout(new BorderLayout());
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        frame.addWindowListener(new WindowAdapter() {
+            @Override public void windowClosing(WindowEvent e) {
+                frame.dispose();
+                System.exit(0);
+            }
+        });
+    }
+
     public static void setCurrent(Canvas canvas) {
-        // stub
+        init();
+
+        if (current == canvas) return;
+
+        if (current != null) {
+            frame.remove(current.unwrap());
+        }
+
+        current = canvas;
+
+        if (canvas != null) {
+            frame.add(canvas.unwrap(), BorderLayout.CENTER);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+
+            canvas.unwrap().requestFocus();
+        }
+    }
+
+    public static Canvas getCurrent() {
+        return current;
     }
 }
