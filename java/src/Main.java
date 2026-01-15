79cba4c26d964d7da643adde78fb6ec0bff14f6b
tsgrgo <37509858+tsgrgo@users.noreply.github.com>
2026-01-15 19:45:25 +0100

    Import scratchpad file properly

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,16 +1,29 @@
 import com.nttdocomo.ui.Display;
 
 import javax.microedition.io.Connector;
-import java.awt.Frame;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
-    public static void main(String[] args) {
-        Connector.setScratchpadFile(new File(""));
+    private static final String SCRATCHPAD = "ENGLISH_PATCH__Tamagotchi_Park.sp";
 
-        GameApp app = new GameApp();
+    public static void main(String[] args) throws IOException {
+        Connector.setScratchpadFile(getScratchpadFile());
+        new GameApp();
         Display.init();
+    }
+
+    private static File getScratchpadFile() throws IOException {
+        Path target = Path.of(System.getProperty("java.io.tmpdir"), Main.SCRATCHPAD);
+        if (Files.exists(target)) return target.toFile();
+
+        try (InputStream in = Main.class.getClassLoader().getResourceAsStream(Main.SCRATCHPAD)) {
+            if (in == null) throw new IOException("Resource not found: " + Main.SCRATCHPAD);
+            Files.copy(in, target);
+        }
+        return target.toFile();
     }
 }
