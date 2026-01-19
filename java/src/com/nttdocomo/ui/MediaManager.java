cdb69b012c3d0ca4d043698f2ddaebb395812501
tsgrgo <37509858+tsgrgo@users.noreply.github.com>
2026-01-19 23:07:08 +0100

    Ugly implementation for getImage(String location)

--- java/src/com/nttdocomo/ui/MediaManager.java
+++ java/src/com/nttdocomo/ui/MediaManager.java
@@ -1,19 +1,49 @@
 package com.nttdocomo.ui;
 
+import javax.microedition.io.Connector;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+
 public class MediaManager {
 
-    public static MediaImage getImage(String s) {
-        byte[] data = new byte[0];
-        // TODO: read from sp
-        System.out.println("MediaManager.getImage: " + s);
+    public static MediaImage getImage(String location) {
+        if (location == null) {
+            throw new NullPointerException("location");
+        }
+
+        if (location.startsWith("comm:") || location.startsWith("obex:")) {
+            throw new IllegalArgumentException("Unsupported location: " + location);
+        }
+
+        try (DataInputStream in = new DataInputStream(Connector.openInputStream(location))) {
+            // Disgusting solution but IDK how to do this better for now...
+            byte[] data = readAll(in);
+            return new MediaImage(data);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load image: " + location, e);
+        }
+    }
+
+    public static MediaImage getImage(byte[] data) {
+        if (data == null) throw new NullPointerException("data");
         return new MediaImage(data);
     }
 
     public static MediaSound getSound(byte[] data) {
+        if (data == null) throw new NullPointerException("data");
         return new MediaSound(data);
     }
 
-    public static MediaImage getImage(byte[] data) {
-        return new MediaImage(data);
+    private static byte[] readAll(DataInputStream in) throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream(4096);
+        byte[] buf = new byte[4096];
+        int n;
+
+        while ((n = in.read(buf)) != -1) {
+            out.write(buf, 0, n);
+        }
+
+        return out.toByteArray();
     }
 }
