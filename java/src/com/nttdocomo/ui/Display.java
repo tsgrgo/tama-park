a78d65adb3ec43841ec6cb03faf0d560be5f303a
tsgrgo <37509858+tsgrgo@users.noreply.github.com>
2026-01-21 16:30:04 +0100

    Move frame creating to IApplication

--- java/src/com/nttdocomo/ui/Display.java
+++ java/src/com/nttdocomo/ui/Display.java
@@ -1,54 +1,7 @@
 package com.nttdocomo.ui;
 
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
 public final class Display {
-
-    private static Canvas current;
-    private static Frame frame;
-
-    private static void init() {
-        if (frame != null) return;
-
-        frame = new Frame("DoJa Emulator");
-        frame.setSize(240, 240);
-        frame.setLayout(new BorderLayout());
-        frame.setResizable(false);
-        frame.setVisible(true);
-
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                frame.dispose();
-                System.exit(0);
-            }
-        });
-    }
-
     public static void setCurrent(Canvas canvas) {
-        init();
-
-        if (current == canvas) return;
-
-        if (current != null) {
-            frame.remove(current.unwrap());
-        }
-
-        current = canvas;
-
-        if (canvas != null) {
-            frame.add(canvas.unwrap(), BorderLayout.CENTER);
-            frame.pack();
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
-
-            canvas.unwrap().requestFocus();
-        }
-    }
-
-    public static Canvas getCurrent() {
-        return current;
+        IApplication.getCurrentApp().setCanvas(canvas);
     }
 }
--- java/src/com/nttdocomo/ui/IApplication.java
+++ java/src/com/nttdocomo/ui/IApplication.java
@@ -1,29 +1,81 @@
 package com.nttdocomo.ui;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
+
 public abstract class IApplication {
+    private static IApplication currentApp;
 
-    protected IApplication() {}
-
-    protected static IApplication getCurrentApp() {
-        return null;
+    private String[] args = new String[0];
+    private Canvas canvas;
+    private Frame frame;
+    
+    protected IApplication() {
+        currentApp = this;
+        createFrame();
     }
 
-    public void start() {}
-    public void stop() {}
-    public void resume() {}
-    public void suspend() {}
+    protected static IApplication getCurrentApp() {
+        return currentApp;
+    }
+
+    public abstract void start();
+
+    public abstract void resume();
 
     protected String getSourceURL() {
         return "";
     }
 
     protected String[] getArgs() {
-        return new String[0];
+        return (args == null) ? new String[0] : Arrays.copyOf(args, args.length);
     }
 
-    public void launch(int i, String[] var2) {
+    public void launch(int type, String[] args) {
+        if (args != null) this.args = Arrays.copyOf(args, args.length);
+        System.out.println("IApplication.launch type=" + type + " args=" + Arrays.toString(this.args));
     }
 
     public void terminate() {
+        System.out.println("terminated");
+        if (frame != null) frame.dispose();
+        System.exit(0);
     }
+
+    void setCanvas(Canvas canvas) {
+        if (this.canvas == canvas) return;
+
+        if (this.canvas != null) {
+            frame.remove(this.canvas.unwrap());
+        }
+
+        this.canvas = canvas;
+
+        if (canvas != null) {
+            frame.add(canvas.unwrap(), BorderLayout.CENTER);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+            canvas.unwrap().requestFocus();
+        }
+    }
+
+    private void createFrame() {
+        if (frame != null) return;
+
+        frame = new Frame("DoJa Emulator");
+        frame.setSize(240, 240);
+        frame.setLayout(new BorderLayout());
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                terminate();
+            }
+        });
+    }
+
 }
