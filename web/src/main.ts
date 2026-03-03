8ee442a974010d56338969b47feb7b980e42e028
tsgrgo <37509858+tsgrgo@users.noreply.github.com>
2026-03-03 15:53:00 +0100

    Add animated favicon

--- web/src/main.ts
+++ web/src/main.ts
@@ -6,3 +6,21 @@ IApplication.setSourceURL('/data/');
 // Canvas.setTftFilterEnabled(true);
 
 const game = new GameApp();
+
+startIconAnimation(['icons/image_36.png', 'icons/image_35.png'], 1000);
+
+function startIconAnimation(icons: string[], speed: number) {
+	let iconIndex = 0;
+
+	setInterval(() => {
+		[...document.querySelectorAll('[rel="icon"]')].forEach(e => e.remove());
+
+		if (iconIndex >= icons.length) iconIndex = 0;
+		const link = document.createElement('link');
+		link.rel = 'icon';
+		link.href = icons[iconIndex++];
+		link.type = 'image/png';
+
+		document.head.append(link);
+	}, speed);
+}
