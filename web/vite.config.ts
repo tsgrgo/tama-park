6aff6588d45422874e67cfc475d20659a14fec5f
tsgrgo <37509858+tsgrgo@users.noreply.github.com>
2026-07-25 13:32:40 +0200

    Add offline PWA support

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+	plugins: [
+		VitePWA({
+			registerType: 'autoUpdate',
+			injectRegister: 'auto',
+			manifest: false,
+			workbox: {
+				cleanupOutdatedCaches: true,
+				globPatterns: [
+					'**/*.{js,css,html,png,svg,woff2,bin,webmanifest}'
+				],
+				navigateFallback: '/index.html'
+			}
+		})
+	],
 	server: {
 		headers: {
 			'Cross-Origin-Opener-Policy': 'same-origin',
